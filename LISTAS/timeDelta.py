c57721ebcd25c94927284fe69c5756c3b2441145
Junior de Freitas <juniordefreitas1@gmail.com>
2025-03-12 19:59:16 -0300

    date time

--- LISTAS/timeDelta.py
+++ LISTAS/timeDelta.py
@@ -1,18 +1,27 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date,time
 
-tamanhoCarro = 'p'
+tamanhoCarro = 'g'
 tempo_P = 30
 tempo_M = 45
-tempo_G = 60
+tempo_G = 5
 data_atual = datetime.now()
 
 
 if tamanhoCarro == 'p':
-    print(data_atual)
+    data_estimada = data_atual + timedelta(minutes=(tempo_P))
+    print(f'O carro chegou as: {data_atual}\ne ficará pronto as: {data_estimada}')
 
-if tamanhoCarro == 'm':
-    print(data_atual)
+elif tamanhoCarro == 'm':
+    
+    data_estimada = data_atual + timedelta(minutes=(tempo_M))
+    print(f'O carro chegou as: {data_atual} e ficará pronto as: {data_estimada}')
 
 
-if tamanhoCarro == 'g':
-    print(data_atual)
+else:
+    data_estimada = data_atual + timedelta(days=(tempo_G))
+    print(f'O carro chegou as: {data_atual}\ne ficará pronto as: {data_estimada}')
+
+
+print(date.today() - timedelta(days=2))
+resultado = (datetime(2025,7,25,10,19,20) - timedelta(hours=1))#para ,manipular a hora ou data joga data/hora completa e faz a operação
+print(resultado.time()) 
