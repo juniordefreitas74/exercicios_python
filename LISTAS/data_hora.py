efbb13aff4a48ff2d48e3e511c8d827f5659be64
juniordefreitas <juniordefreitas1@gmail.com>
2025-03-12 16:55:38 -0300

    date time

---
+++ LISTAS/data_hora.py
@@ -0,0 +1,15 @@
+# vc está importando datetime como date
+import datetime
+from datetime import date, datetime, time, timedelta
+
+data = date(1974, 9, 16)
+print(data)
+
+
+print(date.today())
+
+print(datetime.now())
+print(datetime.today())
+print(time(10, 20, 30))
+print(time())
+
---
+++ LISTAS/timeDelta.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+
+tamanhoCarro = 'p'
+tempo_P = 30
+tempo_M = 45
+tempo_G = 60
+data_atual = datetime.now()
+
+
+if tamanhoCarro == 'p':
+    print(data_atual)
+
+if tamanhoCarro == 'm':
+    print(data_atual)
+
+
+if tamanhoCarro == 'g':
+    print(data_atual)
