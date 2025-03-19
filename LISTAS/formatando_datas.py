f7b0ad6d5e4b60c19ade8ab2ab4305d80afe4467
Junior de Freitas <juniordefreitas1@gmail.com>
2025-03-19 20:54:13 -0300

    data convertida

---
+++ LISTAS/formatando_datas.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import locale
+
+# Define o idioma para português do Brasil
+locale.setlocale(locale.LC_TIME, "pt_BR.utf8")
+
+data_atual = datetime.now()
+data_string = '2025-03-19 20:37'
+
+mascara_ptbr = '%A %d/%h/%Y %H:%M ' #criei uma variavel com o formato da mascara que quero apresentar
+mascara_en = '%Y-%m-%d %H:%M'
+print(data_atual.strftime(mascara_ptbr))
+
+data_convertida = datetime.strptime(data_string, mascara_en)
+print(data_convertida)
+
+
--- LISTAS/timeDelta.py
+++ LISTAS/timeDelta.py
@@ -8,30 +8,13 @@ data_atual = datetime.now()
 
 
 if tamanhoCarro == 'p':
-<<<<<<< HEAD
     # o time delta vc escolhe o que vc quer somar ou subtrair, neste caso minutos
     data_estimada = data_atual + timedelta(minutes=tempo_P)
     print(data_atual)
     print(data_estimada)
-    print(
-        f'O carro chegou as: {data_atual} e fiacará pronto as{data_estimada}')
+    print(f'O carro chegou as: {data_atual} e fiacará pronto as{data_estimada}')
 
-if tamanhoCarro == 'm':
-    data_estimada = data_atual + timedelta(minutes=tempo_M)
-    print(data_atual)
-    print(data_estimada)
-    print(
-        f'O carro chegou as: {data_atual} e fiacará pronto as{data_estimada}')
 
-if tamanhoCarro == 'g':
-    data_estimada = data_atual + timedelta(minutes=tempo_G)
-    print(data_atual)
-    print(data_estimada)
-    print(
-        f'O carro chegou as: {data_atual} e fiacará pronto as{data_estimada}')
-=======
-    data_estimada = data_atual + timedelta(minutes=(tempo_P))
-    print(f'O carro chegou as: {data_atual}\ne ficará pronto as: {data_estimada}')
 
 elif tamanhoCarro == 'm':
     
@@ -47,4 +30,3 @@ else:
 print(date.today() - timedelta(days=2))
 resultado = (datetime(2025,7,25,10,19,20) - timedelta(hours=1))#para ,manipular a hora ou data joga data/hora completa e faz a operação
 print(resultado.time()) 
->>>>>>> c57721ebcd25c94927284fe69c5756c3b2441145
