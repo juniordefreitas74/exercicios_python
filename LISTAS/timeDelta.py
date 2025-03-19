88eb595655a29f14ee918a825bd36a86c9744195
juniordefreitas <juniordefreitas1@gmail.com>
2025-03-19 14:26:24 -0300

    time delta

--- LISTAS/timeDelta.py
+++ LISTAS/timeDelta.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-tamanhoCarro = 'p'
+tamanhoCarro = 'g'
 tempo_P = 30
 tempo_M = 45
 tempo_G = 60
@@ -8,11 +8,23 @@ data_atual = datetime.now()
 
 
 if tamanhoCarro == 'p':
+    # o time delta vc escolhe o que vc quer somar ou subtrair, neste caso minutos
+    data_estimada = data_atual + timedelta(minutes=tempo_P)
     print(data_atual)
+    print(data_estimada)
+    print(
+        f'O carro chegou as: {data_atual} e fiacará pronto as{data_estimada}')
 
 if tamanhoCarro == 'm':
+    data_estimada = data_atual + timedelta(minutes=tempo_M)
     print(data_atual)
-
+    print(data_estimada)
+    print(
+        f'O carro chegou as: {data_atual} e fiacará pronto as{data_estimada}')
 
 if tamanhoCarro == 'g':
+    data_estimada = data_atual + timedelta(minutes=tempo_G)
     print(data_atual)
+    print(data_estimada)
+    print(
+        f'O carro chegou as: {data_atual} e fiacará pronto as{data_estimada}')
